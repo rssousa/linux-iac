404b1197416c17ad04da108a0bebf7f60f6b9e3c
rssousa <souromulo@hotmail.com>
2022-11-12 22:46:20 -0300

    Create iac2.sh

---
+++ iac2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Atualizando o servidor"
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install 
+echo "Baixando e copiando arquivos da aplicação"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
