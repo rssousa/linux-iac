f338009ec1949d65422cfaa1c0c921e3d463ed39
rssousa <souromulo@hotmail.com>
2022-11-14 09:07:00 -0300

    Update iac2.sh

--- iac2.sh
+++ iac2.sh
@@ -9,5 +9,5 @@ echo "Baixando e copiando arquivos da aplicação"
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
-cd linux-site-dio
+cd linux-site-dio-main
 cp -R * /var/www/html/
